2d21f47f5126098c9004b8000f0d3405b67087c7
Sayed Mubarish <mubarish007@gmail.com>
2025-10-30 16:59:33 +0530

    feat:chunking and embedding

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from src.data_loader import load_all_documents
-
+from src.embedding import EmbeddingPipeline
 
 if __name__ == "__main__":
     docs = load_all_documents("data")
-    
+    emb_pipe = EmbeddingPipeline()
+
+    chunks = emb_pipe.chunk_documents(docs)
+    embeddings = emb_pipe.embed_chunks(chunks)
+    print(len(chunks))
+    print(len(embeddings))
